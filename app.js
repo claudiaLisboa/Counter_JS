21824f2b38b7b16c710841711160248a8955d6e0
Claudia Lisboa <ccalumby@gmail.com>
2022-02-12 22:12:28 -0500

    Update app.js
    select all the buttons are used for each

---
+++ app.js
@@ -0,0 +1,24 @@
+//set inital count
+let count = 0;
+
+//select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+//I select all the buttons are used for each, 
+//I looked over those buttons values. 
+//For each button, add an event listener, as I'm clicking, ans I'm using the event object and getting the current target and a class list. So getting me the classes that the item that I'm clicking - button : decrease, reset, increase
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function(c){
+        const styles = c.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count--;
+        }else if(styles.contains("increase")){
+            count++;
+        }else{
+            count =0;
+        }
+
+        value.textContent = count;
+        });
+});
