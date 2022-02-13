cb853021509c67559f8c3ee087a7fe3de1246b95
Claudia Lisboa <ccalumby@gmail.com>
2022-02-12 22:26:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,19 @@ const btns = document.querySelectorAll(".btn");
 btns.forEach(function(btn) {
     btn.addEventListener("click", function(c){
         const styles = c.currentTarget.classList;
-        if(styles.contains("decrease")){
+        if(styles.contains("decrease")){ 
             count--;
         }else if(styles.contains("increase")){
             count++;
         }else{
             count =0;
+        }
+        if (count > 0){
+            value.style.color = "green";
+        } else if (count < 0){
+            value.style.color = "red";
+        }else{
+            value.style.color = "black";
         }
 
         value.textContent = count;
